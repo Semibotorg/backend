7cda403a023414dba4b3a919611c09dff822e63a
Mohamed Ashraf <mohmed6002090@gmail.com>
2023-07-20 16:45:20 +0300

    feat(Auth): setup discord authorization

--- src/api/v1/controllers/auth.controller.ts
+++ src/api/v1/controllers/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Router, Request, Response } from 'express';
 import { autoInjectable } from 'tsyringe';
 import { AuthService } from '../services/auth.service';
+import { scopes } from '../../../config/constants';
 
 @autoInjectable()
 export class AuthController {
 	AuthService: AuthService;
 	router: Router;
+	envData = process.env;
 	constructor(AuthService: AuthService) {
 		this.AuthService = AuthService;
 		this.router = Router();
@@ -16,8 +18,30 @@ export class AuthController {
 		return response.status(200).send({ text });
 	}
 
+	redirectToLoginPage(request: Request, response: Response) {
+		response.redirect(this.envData.DISCORD_AUTH_URL);
+	}
+
+	async authorizeUser(request: Request, response: Response) {
+		const code = request.query.code;
+		const error = request.query.error;
+		if (error) return response.status(200).send(`<script>window.close();</script>`);
+		if (!code) return response.status(400).send({ msg: 'wrong code' });
+
+		const data = await this.AuthService.getAccessToken(
+			this.envData.DISCORD_CLIENT_ID,
+			this.envData.DISCORD_CLIENT_SECRET,
+			this.envData.DISCORD_REDITECT_URL,
+			scopes,
+			code,
+		);
+		console.log(data);
+		return response.status(200).send(data);
+	}
+	
 	routes() {
-		this.router.get('/', (request, response) => this.getAuthRoute(request, response));
+		this.router.get('/login', (request, response) => this.redirectToLoginPage(request, response));
+		this.router.get('/callback', async (request, response) => await this.authorizeUser(request, response));
 		return this.router;
 	}
 }
--- src/api/v1/services/auth.service.ts
+++ src/api/v1/services/auth.service.ts
@@ -1,9 +1,32 @@
 import { autoInjectable } from 'tsyringe';
-
+import { RESTPostOAuth2AccessTokenResult, Routes } from 'discord-api-types/v10';
+import axios from 'axios';
+import { DISCORD_API_URL, DISCORD_API_VERSION } from '../../../config/constants';
 @autoInjectable()
 export class AuthService {
 	constructor() {}
 	getHelloWorld() {
 		return 'hello world';
+	}
+
+	async getAccessToken(clientId, clientSecret, redirectUrl, scopes: string[], code) {
+		const data = new URLSearchParams();
+		data.append('client_id', clientId);
+		data.append('client_secret', clientSecret);
+		data.append('grant_type', 'authorization_code');
+		data.append('redirect_uri', redirectUrl);
+		data.append('scope', scopes.join(' '));
+		data.append('code', code);
+
+		const exchangeDiscordToken = await axios.post(
+			`${DISCORD_API_URL}/${DISCORD_API_VERSION}${Routes.oauth2TokenExchange()}`,
+			data.toString(),
+			{
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+			},
+		);
+		return exchangeDiscordToken.data as RESTPostOAuth2AccessTokenResult;
 	}
 }
