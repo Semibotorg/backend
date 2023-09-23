1de9b44fe1a03c4449955b4ea328658c26235e03
dager-mohamed <mohmed6002090@gmail.com>
2023-09-23 14:49:12 +0300

    feat(RateLimit): adding rate limit for the application

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import { config } from 'dotenv';
 config();
 import express from 'express';
+import { rateLimit } from 'express-rate-limit';
 import cors from 'cors';
 import { MainRoutes } from './api/v1/routes';
 import { handleErrors } from './utils';
@@ -13,6 +14,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+// Add rate limiting middleware
+const limiter = rateLimit({
+	windowMs: 60 * 60 * 1000,
+	max: 100,
+	message: 'You have exceeded the 100 requests in 1 hour limit!',
+	standardHeaders: true,
+	legacyHeaders: false,
+});
+app.use(limiter);
 
 handleErrors();
 runCron();
